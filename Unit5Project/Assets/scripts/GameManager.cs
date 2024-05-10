b7713d6919df58acc4a7695c925a5548da7d95ed
Mariana <nano091306@gmail.com>
2024-05-10 13:46:19 -0700

    finished the project just doing the extra credit

--- Unit5Project/Assets/scripts/GameManager.cs
+++ Unit5Project/Assets/scripts/GameManager.cs
@@ -2,24 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public Button restartButton;
     public bool isGameActive;
+    public GameObject titleScreen;
+    public TextMeshProUGUI livesText;
     private int score;
     private float spawnRate = 1.0f;
+    private int lives;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
+      
     }
 
     // Update is called once per frame
@@ -27,7 +31,6 @@ public class GameManager : MonoBehaviour
     {
         
     }
-
     IEnumerator SpawnTarget()
     {
         while (isGameActive)
@@ -40,6 +43,16 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public void UpdateLives(int livesToChange)
+    {
+        lives += livesToChange;
+        livesText.text = "Lives: " + lives;
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+    }
+
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
@@ -48,7 +61,25 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
+        restartButton.gameObject.SetActive(true);
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate = spawnRate / difficulty;
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        UpdateLives(3);
+
+        titleScreen.gameObject.SetActive(false);
     }
 }
